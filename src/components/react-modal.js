ad6edbe7ab7bf8c9fcd26602d41830197e74da34
afeiship <1290657123@qq.com>
2017-04-17 23:27:30 +0800

    bugfix

--- src/components/react-modal.js
+++ src/components/react-modal.js
@@ -54,7 +54,7 @@ export default class ReactModal extends ReactVisible{
 
   show(inOptions,inCallback){
     const {root} = this.refs;
-    const options = Object.assign({...this.props},inOptions);
+    const options = Object.assign({...this.props},{hidden:false},inOptions);
     this.setState( options ,()=>{
       measureIt(root,(dimensions) => {
         this.setState({ dimensions },()=>{
